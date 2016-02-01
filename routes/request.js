fc0a9c9613d13c18506813110e98b519703a51de
pengfei.zhou <pengfei.zhou@dianping.com>
2016-02-01 19:29:50 +0800

    json以及multipart的POST转发

--- routes/request.js
+++ routes/request.js
@@ -6,29 +6,56 @@ var router = express.Router();
 //url=?&sessionid=?
 var http = require('http');
 var URL = require('url');
-router.get('/', function (req, res, next) {
+var handler = function (req, res, next) {
     var urlP = URL.parse(req.query.url);
+    var headers = {};
+    for (var i in req.headers) {
+        if (!/host|connection/i.test(i)) {
+            headers[i] = req.headers[i];
+        }
+    }
+    var bodyString = JSON.stringify(req.body);
     var options = {
         hostname: urlP.hostname,
         port: urlP.port,
         path: urlP.path,
         method: req.method,
-        headers: req.headers
+        headers: headers
     };
     var sessionid = req.query.sessionid;
     var req2 = http.request(options, function (res2) {
         res.writeHead(res2.statusCode, res2.headers);
         res2.pipe(res);
+        res2.on('data', function (data) {
+            console.log("request " + req.query.url + " data:" + data);
+        });
     });
     if (/POST|PUT/i.test(req.method)) {
-        req.pipe(req2);
+        if (req.headers['content-type'] &&
+            req.headers['content-type'].indexOf('multipart/form-data') === 0 &&
+            (req.method === 'POST' || req.method === 'PUT')){
+            var size = 0;
+            req.on('data', function (data) {
+                req2.write(data);
+            });
+            req.on('end', function () {
+                req2.end();
+            });
+        }else{
+            req2.write(req.rawBody);
+        }
     } else {
         req2.end();
     }
     req2.on('error', function (e) {
-        console.log("request req.query.url error: " + e.message);
+        console.log("request " + req.query.url + " error:" + e.message);
         res.end(e.stack);
     });
-});
+};
+
+router.get('/', handler);
+router.post('/', handler);
+router.put('/', handler);
+router.delete('/', handler);
 
 module.exports = router;
